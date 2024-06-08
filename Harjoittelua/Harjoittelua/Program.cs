e9137e14dce8879816442dfdef1591f20f4ad73a
Traizero <43611184+Traizero@users.noreply.github.com>
2024-06-08 20:13:41 +0300

    jalkapallo

--- Harjoittelua/Harjoittelua/Program.cs
+++ Harjoittelua/Harjoittelua/Program.cs
@@ -1,2 +1,21 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+namespace Harjoittelua
+{
+    public class Game
+    {
+        public int hp = 200;
+        //for  (int i = 0; i< 10; i++;)
+
+           // {
+            
+            //}
+            //ba
+        //player has data, hand, player has balls, football, soccer, yes, storage has balls, ball has durability/air... if durability reaches zero... the ball pops. Fill air in? y/n
+
+
+    }
+
+}
+

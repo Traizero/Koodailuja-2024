b1b02cf9e31d592d5a8b2c4f831ebc688cc69bc6
Traizero <43611184+Traizero@users.noreply.github.com>
2024-06-09 17:27:55 +0300

    harjoittelua pushi

---
+++ Harjoittelua/Harjoittelua/Ball.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Harjoittelua
+{
+    public class Ball
+    {
+        int ballHp = 100;
+        
+    }
+}
